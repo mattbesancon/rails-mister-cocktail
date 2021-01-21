5abaad807fab146d3f59e23bf51a86b40ab252a9
Matthieu Besancon <matthieu.besancon@gmail.com>
2021-01-21 16:44:38 +0100

    add relation in ingredient model & permit ingredient_id in doses controller

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -20,6 +20,7 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
+
