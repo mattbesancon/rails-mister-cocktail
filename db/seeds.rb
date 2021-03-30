ab3c45d2764f50eb1293ca9d434589a26b44908e
Matthieu Besancon <matthieu.besancon@gmail.com>
2021-03-30 10:19:16 +0200

    going back to looking by name in the cocktaildb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,6 @@ require 'json'
 require 'rest-client'
 
 
-
 (0..9).each do |i|
     result = RestClient.get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=1100#{i}")
     response = JSON.parse(result)
@@ -20,9 +19,7 @@ require 'rest-client'
             el = x["strIngredient#{j}"] 
             if el.nil? == false && Ingredient.find_by_name(el).nil?
                 new_ing = Ingredient.create(name: el)
-                # double interpolation (see probable solution there: https://exceptionshub.com/in-ruby-can-you-perform-string-interpolation-on-data-read-from-a-file-2.html)
-                # using concatenation instead?
-                res = RestClient.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?i="+el)
+                res = RestClient.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?i=#{el}")
                 resp = JSON.parse(res)
                 if resp["ingredients"][0]["strAlcohol"] == "Yes"
                     new_ing.liquor == true
