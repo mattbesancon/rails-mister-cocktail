d0d688b52b27fc99cd8f68b9c7db584b92be3571
Matthieu Besancon <matthieu.besancon@gmail.com>
2021-04-19 10:56:25 +0200

    diff between ingredient with alc and wihtout alc from API to seed the db : DONE

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,13 +16,14 @@ require 'rest-client'
     response["drinks"].each do |x|
         cocktail = Cocktail.create(name: x["strDrink"], photo: x["strDrinkThumb"], description: x["strInstructions"], category: x["strCategory"])
         (1..15).each do |j|
-            el = x["strIngredient#{j}"] 
-            if el.nil? == false && Ingredient.find_by_name(el).nil?
+            el = x["strIngredient#{j}"]
+            if el.nil? == false && Ingredient.find_by_name(el).nil?            
                 new_ing = Ingredient.create(name: el)
                 res = RestClient.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?i=#{el}")
                 resp = JSON.parse(res)
                 if resp["ingredients"][0]["strAlcohol"] == "Yes"
-                    new_ing.liquor == true
+                    new_ing.liquor = true
+                    new_ing.save
                 end 
                 new_dose = Dose.create(description: x["strMeasure#{j}"], cocktail_id: cocktail.id, ingredient_id: new_ing.id)               
             end
@@ -31,22 +32,24 @@ require 'rest-client'
 end
 
 
+
 =begin
 (10..60).each do |i|
     result = RestClient.get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=1100#{i}")
     response = JSON.parse(result)
     response["drinks"].each do |x|
-        cocktail = Cocktail.create!(name: x["strDrink"], photo: x["strDrinkThumb"], description: x["strInstructions"], category: x["strCategory"])
-        (1..15).each do |i|
-            el = x["strIngredient#{i}"] 
-            if el != nil
-                new_ing = Ingredient.create!(name: el)
+        cocktail = Cocktail.create(name: x["strDrink"], photo: x["strDrinkThumb"], description: x["strInstructions"], category: x["strCategory"])
+        (1..15).each do |j|
+            el = x["strIngredient#{j}"]
+            if el.nil? == false && Ingredient.find_by_name(el).nil?            
+                new_ing = Ingredient.create(name: el)
                 res = RestClient.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?i=#{el}")
                 resp = JSON.parse(res)
                 if resp["ingredients"][0]["strAlcohol"] == "Yes"
-                    new_ing.liquor == true
-                end  
-                new_dose = Dose.create!(description: x["strMeasure#{i}"], cocktail_id: cocktail.id, ingredient_id: new_ing.id)
+                    new_ing.liquor = true
+                    new_ing.save
+                end 
+                new_dose = Dose.create(description: x["strMeasure#{j}"], cocktail_id: cocktail.id, ingredient_id: new_ing.id)               
             end
         end
     end
