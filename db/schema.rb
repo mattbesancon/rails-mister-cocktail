9376fdc54508635603b743b273fd131cc2a39cd1
Matthieu Besancon <matthieu.besancon@gmail.com>
2021-04-20 12:01:03 +0200

    adding migration to ingredients for photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_085743) do
+ActiveRecord::Schema.define(version: 2021_04_20_100029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_04_16_085743) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "liquor", default: false
+    t.string "photo"
   end
 
   create_table "pg_search_documents", force: :cascade do |t|
