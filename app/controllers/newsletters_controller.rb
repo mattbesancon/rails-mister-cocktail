ed2235858ee2a5459d81444f6a6ae80085e85fc7
Matthieu Besancon <matthieu.besancon@gmail.com>
2021-06-02 11:11:45 +0200

    controller crud + send method & views for newsletters

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,7 +1,51 @@
 class NewslettersController < ApplicationController
+  before_action :set_newsletter, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @newsletters = Newsletter.all
+  end
+
+  def new
+    @newsletter = Newsletter.new
+  end
+
   def create
+    @newsletter = Newsletter.new(newsletter_params)
+    @newsletter.save
+    redirect_to newsletters_path(@newsletter)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @newsletter = Newsletter.update(newsletter_params)
+    redirect_to newsletters_path(@newsletter)
+  end
+
+  def destroy
+    @newsletter.destroy
+    redirect_to newsletters_path
   end
 
   def send
+    @newsletter = Newsletter.find(:params[:id])
+    @users = User.all
+    @users.each do |user|
+      Newsletter.newsletter_email(user, @newsletter).deliver
+    end
+  end
+
+  private
+
+  def set_newsletter
+    @newsletter = Newsletter.find(params[:id])
+  end
+
+  def newsletter_params
+    params.require(:newsletter).permit(:email)
   end
 end
