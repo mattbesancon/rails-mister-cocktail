053c3aae653834488af6c4bc50efef73e5be2215
Matthieu Besancon <matthieu.besancon@gmail.com>
2021-06-07 15:45:11 +0200

    generate mail for newsletter & first draft

---
+++ app/mailers/newsletter_mailer.rb
@@ -0,0 +1,8 @@
+class NewsletterMailer < ApplicationMailer
+  default :from => "rails-mister-cocktail@gmail.com", :content_type => "multipart/mixed"
+
+  def newsletter_email(newsletter)
+    @newsletter = newsletter
+    mail(:to => newsletter.email, :subject => newsletter.subject)
+  end
+end
