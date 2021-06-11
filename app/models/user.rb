3b52d71c8dc780822d9b8cf93cb1ef12a766b323
Matthieu Besancon <matthieu.besancon@gmail.com>
2021-06-11 11:18:17 +0200

    not need to connect user & newsletter email --just send to the email provided

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   ratyrate_rater
-  validates :email, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
