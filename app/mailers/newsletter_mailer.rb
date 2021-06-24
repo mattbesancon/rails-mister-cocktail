be1da3791a074bd0e372b9d36976cb84332fa693
Matthieu Besancon <matthieu.besancon@gmail.com>
2021-06-24 11:28:42 +0200

    todo when deploy : integrate mailgun with personnal email

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,5 +1,6 @@
 class NewsletterMailer < ApplicationMailer
   default :from => "rails-mister-cocktail@gmail.com"
+  # TODO: see how to integrate mailgun & use my personnal email address
 
   def newsletter_email(newsletter)
     @newsletter = newsletter
