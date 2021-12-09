1a8d3fa1371c6106e7f5044a1cdfa435a706acec
Matthieu Besancon <matthieu.besancon@gmail.com>
2021-12-09 16:42:08 +0100

    first draft: example from toptal

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,4 +4,8 @@ class Ingredient < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :cocktails, through: :doses
   has_many :doses
+
+  def image_urls
+    Ingredient.all.select { |m| m.liquor == true }
+  end
 end
