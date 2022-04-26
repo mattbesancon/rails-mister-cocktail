ba785666492bc23dd016744b2b99c441e0515395
Matthieu Besancon <matthieu.besancon@gmail.com>
2022-04-26 18:05:23 +0200

    remove image_url from ingredient model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,8 +4,5 @@ class Ingredient < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :cocktails, through: :doses
   has_many :doses
-
-  def image_urls
-    Ingredient.all.select { |m| m.liquor == true }.map { |m| m.photo }
-  end
 end
+
